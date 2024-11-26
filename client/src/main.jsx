ab80b37bd82a27034bb56ff39de58ef71ee4bd48
Sarah Zwart <sarahczwart@gmail.com>
2024-11-26 13:36:12 -0500

    got google oauth working'

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+const clientId = '206857669848-6v63ti65j7this7e1j8lvvhhbha6r9m1.apps.googleusercontent.com'; 
+
 createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId='1160834342166-0fqbjlltdtski3oo9no01dpmhc7c87qn.apps.googleusercontent.com'>
+  <GoogleOAuthProvider clientId={clientId}>
     <StrictMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </StrictMode>
-  </GoogleOAuthProvider>,
-)
+  </GoogleOAuthProvider>
+);
--- server1/server.js
+++ server1/server.js
@@ -1,16 +1,40 @@
 const express = require("express");
+const dotenv = require('dotenv')
 const app = express();
 const cors = require("cors");
 const corsOptions = {
-    orgin: ["http://localhost:5173"]
+    origin: ["http://localhost:5173"],
 }
 app.use(cors(corsOptions))
-
+app.use(express.json());
+dotenv.config();
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 app.listen(8080, () =>{
     console.log("Server started on port 8080");
 });
 
+app.post('/login', async (req, res) => {
+    console.log(req.body);
+    const { token } = req.body; // Get the Google OAuth token from the frontend
+    if (!token) {
+      return res.status(400).send('Token is required');
+    }
+    try {
+      // Verify the token
+      const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.GOOGLE_CLIENT_ID, // Your Google Client ID
+      });
+      const payload = ticket.getPayload();
+      console.log(payload); 
+      res.status(200).send({ message: 'User authenticated', user: payload });
+    } catch (error) {
+      console.error('Error verifying token:', error);
+      res.status(400).send('Invalid token');
+    }
+  });
 /*GET*/
 // route for getting plants by user
 app.get("/users/:userID/plants", (req, res) => {
@@ -19,7 +43,7 @@ app.get("/users/:userID/plants", (req, res) => {
 
 // route for getting plants 
 app.get("/plants", (req, res) => {
-    
+    res.json('hello')
 });
 
 
