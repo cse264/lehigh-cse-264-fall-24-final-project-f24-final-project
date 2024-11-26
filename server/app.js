0bf03fe44678c2106217f8ab94a8968e66c4349a
Layan Suleiman <layansuleiman@dyn223095.cc.lehigh.edu>
2024-11-25 21:24:42 -0500

    add server test

---
+++ server/app.js
@@ -0,0 +1,17 @@
+require ('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+const plantRoutes = require('./routes/plants');
+app.use('/api/plants', plantRoutes);
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+//new edit
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
