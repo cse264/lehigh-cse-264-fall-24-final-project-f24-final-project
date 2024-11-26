b84826058281c817d9067750f4dfef9ac9b73a4d
Layan Suleiman <layansuleiman@dyn223095.cc.lehigh.edu>
2024-11-25 21:50:15 -0500

    server edits

--- server/app.js
+++ server/app.js
@@ -1,15 +1,22 @@
 require ('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const corsOptions = {
+    origin: ["http://localhost:5173"],
+};
 
 const app = express()
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
+app.get("/api", (req, res) => {
+    res.json({plantTest: ["fern", "flower", "weed"]});
+})
+
 // Routes
-const plantRoutes = require('./routes/plants');
-app.use('/api/plants', plantRoutes);
+//const plantRoutes = require('./routes/plants');
+//app.use('/api/plants', plantRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
