7af1ab724d2ef4de8c306ce8d5797b6ca85cbef1
Layan Suleiman <layansuleiman@Layans-MacBook-Pro.local>
2024-11-28 01:29:16 -0500

    added delete and put in api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -28,6 +28,34 @@ export const getPlants = async (id) => {
     }
   };
 
+//delete plants
+export const deletePlants = async (id) => {
+  try {
+    const res = await axios.delete('http://localhost:8080/plants', {
+      params: { id }  
+    });
+    console.log('Plant Deleted:', res.data);
+    return res.data.plants;  
+  } catch (error) {
+    console.error('Error deleting plants:', error);  
+    return [];
+  }
+};
+
+//PUT for watering the plants
+export const waterPlants = async (id) => {
+  try {
+    const res = await axios.put('http://localhost:8080/plants', {
+      params: { id }  
+    });
+    console.log('Plant Watered:', res.data);
+    return res.data.plants;  
+  } catch (error) {
+    console.error('Error watering plants:', error);  
+    return [];
+  }
+};
+
 // Login
 
 
