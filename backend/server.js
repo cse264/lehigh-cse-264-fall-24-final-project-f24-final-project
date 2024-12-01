704e036ca13ef56233e9e2bbdb0b480d54ad4a25
Ken Zhu <kez226@lehigh.edu>
2024-12-01 18:42:04 -0500

    added review routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./db');
 const userRoutes = require('./routes/users');
+const reviewRoutes = require('./routes/review');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,5 +11,6 @@ connectDB();
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
+app.use('/api/reviews', reviewRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
