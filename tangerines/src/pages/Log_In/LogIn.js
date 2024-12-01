cfc95fd9f5c30260df7ea02bb9af5b8c89a56655
Michele De Guzman Murata <106496883+mmurata22@users.noreply.github.com>
2024-12-01 02:47:02 -0500

    LogIn Page Start

---
+++ tangerines/src/pages/Log_In/LogIn.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './LogIn.css';
+import LogoImage from '../../assets/images/Logo Header.svg';
+//import { Link } from 'react-router-dom';
+
+function LogIn(){
+    return(
+        <div>
+            <div class="card">
+                <img src={LogoImage} class="logo" alt="Tangerines Logo" />
+
+                <h1 class="loginHeader">Log In</h1>
+
+                <form>
+                    {/* Email input */}
+                    <div class="form-group">
+                        <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter Email" />
+                    </div>
+
+                    {/* Password input */}
+                    <div class="form-group">
+                        <input type="password" class="form-control" id="inputPassword" placeholder="Enter Password" />
+                    </div>
+
+                    {/* Submit/Log In */}
+                    <button type="submit" class="btn btn-primary">Log In</button>
+
+                    <p class="or">or</p>
+
+                    <p class="signUp">Dont have an account? Sign Up</p>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default LogIn;
