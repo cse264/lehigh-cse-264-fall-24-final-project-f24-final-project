a4d445c24932423911491eb2b10ac106a2c611f0
Bernice Wong <bew225@lehigh.edu>
2024-12-02 09:44:45 -0500

    modified creation of user

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,6 +4,8 @@ const { v4: uuidv4 } = require('uuid'); // Import UUID function
 const userSchema = new mongoose.Schema({
     userID: { type: String, required: true, unique: true, default: () => uuidv4() }, 
     password: { type: String, required: true },
+    email: {type: String, required: true},
+    name: {type: String, required: true},
     accountType: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
 });
