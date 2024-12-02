df7995323d054a6d1b8ee0d2f0a918f0bb43f6dd
NathanielDiCicco <nad426@lehigh.edu>
2024-12-02 17:35:40 -0500

    login functionality

---
+++ tangerines/src/firebase.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDuJ4ZqhkRzuazpYoZBwQv2maRFznAlNcI",
+  authDomain: "tangerines-7f2c8.firebaseapp.com",
+  projectId: "tangerines-7f2c8",
+  storageBucket: "tangerines-7f2c8.firebasestorage.app",
+  messagingSenderId: "1051755758274",
+  appId: "1:1051755758274:web:2e2f3780c17cb932abf15b",
+  measurementId: "G-XM0F0L8PKT"
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage().ref(); //.ref() is a reference to the root of our bucket
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, storage, provider, firebase };
--- tangerines/src/pages/Log_In/LogIn.js
+++ tangerines/src/pages/Log_In/LogIn.js
@@ -1,79 +1,206 @@
 // NOTE: SIGN UP BACK BUTTON DOESN'T WORKx
 
-import React from 'react';
-import { useState } from 'react';
-import './LogIn.css';
-import LogoImage from '../../assets/images/Logo Header.svg';
+import React from "react";
+import { useState } from "react";
+import "./LogIn.css";
+import LogoImage from "../../assets/images/Logo Header.svg";
+import { db, auth, provider, firebase } from "../../firebase";
 
-function LogIn(){
-    const [isSignUp, setIsSignUp] = useState(false);
-    const [isSignUpVisible, setIsSignUpVisible] = useState(true);
+function LogIn() {
+  const [isSignUp, setIsSignUp] = useState(false);
+  const [isSignUpVisible, setIsSignUpVisible] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-    const toggleForm = () => {
-        setIsSignUp(!isSignUp);
-    };
+  const toggleForm = () => {
+    setIsSignUp(!isSignUp);
+  };
 
-    const hideSignUp = () => {
-        setIsSignUpVisible(false);
-    };
+  const hideSignUp = () => {
+    setIsSignUp(false); 
+    setIsSignUpVisible(true);
+  };
 
-    return(
-        <div>
-            {isSignUpVisible && (
-            <div class="card">
-                <img src={LogoImage} class="logo" alt="Tangerines Logo" />
-                <h1 class="loginHeader">Log In</h1>
+  const handleGoogleSignIn = async () => {
+    try {
+      const result = await auth.signInWithPopup(provider);
+      const user = result.user;
 
-                <form>
-                    {/* Email input */}
-                    <div class="form-group">
-                        <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter Email" />
-                    </div>
+      console.log("User:", user);
+      const userRef = db.collection("users").doc(user.email); //Reference to location in database
+      const doc = await userRef.get();
+      if (!doc.exists) {
+        //checking if user exists
+        await userRef.set({
+          //setting user data
+          uid: user.uid,
+          email: user.email,
+          shoppingList: [],
+          admin: false,
+          allergies: [],
+          likedRecipes: [],
+          savedRecipies: [],
+          createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        });
+      }
 
-                    {/* Password input */}
-                    <div class="form-group">
-                        <input type="password" class="form-control" id="inputPassword" placeholder="Enter Password" />
-                    </div>
+      console.log("User signed in and data stored:", user);
+    } catch (error) {
+      console.error("Google Sign-In error:", error.message);
+    }
+  };
 
-                    <p class="error">Username/Password not found. Please try again.</p>
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError("");
 
-                    {/* Submit/Log In */}
-                    <button type="submit" class="btn btn-primary">Log In</button>
+    try {
+      await auth.signInWithEmailAndPassword(email, password); //uses firebase auth to sign in
+      console.log("User logged in");
+    } catch (error) {
+      setError(error.message); // Display any errors
+      console.error("Error logging in:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-                    {/* Sign Up */}
-                    <p class="or">or</p>
-                    <p class="signUp">Dont have an account? <span class="signUpLink" onClick={toggleForm}>Sign Up</span></p>
-                </form>
-                </div>
-            )}
-                
+  const handleSignUp = async (e) => {
+    e.preventDefault();
 
-                {/* SignUp Overlay */}
-                {isSignUp && (
-                    <div class="overlay">
-                        <div class="overlay-form">
-                            <h2>Sign Up</h2>
-                            <form>
-                                {/* Sign Up */}
-                                <div class="form-group">
-                                    <input type="email" class="form-control" id="signupEmail" aria-describedby="emailHelp" placeholder="Enter Email" />
-                                </div>
-                                <div>
-                                    <input type="password" class="form-control" id="signupPassword" placeholder="Enter Password" />
-                                </div>
+    try {
+      const userCredential = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      ); //firebase Auth
+      const user = userCredential.user;
 
-                                <button type="submit" class="btn btn-primary">
-                                    Sign Up
-                                </button>
+      await db.collection("users").doc(user.email).set({
+        uid: user.uid,
+        email: user.email,
+        shoppingList: [],
+        admin: false,
+        allergies: [],
+        likedRecipes: [],
+        savedRecipies: [],
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      });
 
-                                <p class="backToLogin" onClick={hideSignUp}>Back to Log In</p>
-                            </form>
-                        </div>
-                    </div>
-                )}
+      console.log("User signed up:", user);
+    } catch (error) {
+      setError(error.message);
+      console.error("Error signing up:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  return (
+    <div>
+      {isSignUpVisible && (
+        <div class="card">
+          <img src={LogoImage} class="logo" alt="Tangerines Logo" />
+          <h1 class="loginHeader">Log In</h1>
+
+          <form onSubmit={handleLogin}>
+            {/* Email input */}
+            <div>
+              <input
+                type="email"
+                value={email}
+                class="form-control"
+                id="inputEmail"
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Enter Email"
+                required
+              />
             </div>
-    )
+
+            {/* Password input */}
+            <div class="form-group">
+              <input
+                type="password"
+                class="form-control"
+                id="inputPassword"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Enter Password"
+                required
+              />
+            </div>
+
+            <p class="error">Username/Password not found. Please try again.</p>
+
+            {/* Submit/Log In */}
+            <button type="submit" class="btn btn-primary">
+              Log In
+            </button>
+            {/* Google Sign-In */}
+            <p class="or">or</p>
+            <button
+              type="googleSubmit"
+              className="btn btn-primary"
+              onClick={handleGoogleSignIn}
+            >
+              Google Log In
+            </button>
+
+            {/* Sign Up */}
+            <p class="or">or</p>
+            <p class="signUp">
+              Dont have an account?{" "}
+              <span class="signUpLink" onClick={toggleForm}>
+                Sign Up
+              </span>
+            </p>
+          </form>
+        </div>
+      )}
+
+      {/* SignUp Overlay */}
+      {isSignUp && (
+        <div class="overlay">
+          <div class="overlay-form">
+            <h2>Create New Account</h2>
+            <form onSubmit={handleSignUp}>
+              <div>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Email"
+                  required
+                  class="form-control"
+                />
+              </div>
+              <div>
+                <input
+                  type="password"
+                  value={password}
+                  class="form-control"
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Password"
+                  required
+                />
+              </div>
+              {error && <p style={{ color: "red" }}>{error}</p>}
+              <button type="submit" disabled={loading}>
+                {loading ? "Signing up..." : "Sign Up"}
+              </button>
+
+              <p class="or">or</p>
+              <p class="backToLogin" onClick={hideSignUp}>
+                Back to Log In
+              </p>
+            </form>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default LogIn;
