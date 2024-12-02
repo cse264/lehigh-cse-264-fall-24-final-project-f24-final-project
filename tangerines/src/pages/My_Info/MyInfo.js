7ac682cbd08a999b41fd9c12e828e8acea834219
Michele De Guzman Murata <106496883+mmurata22@users.noreply.github.com>
2024-12-02 00:45:33 -0500

    Pages, js and css implemented

---
+++ tangerines/src/pages/My_Info/MyInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import { useState } from 'react';
+import './MyInfo.css';
+
+function MyInfo () {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default MyInfo;
---
+++ tangerines/src/pages/View_Chefs/ViewChefs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import { useState } from 'react';
+import './ViewChefs.css';
+
+function ViewChefs () {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default ViewChefs;
