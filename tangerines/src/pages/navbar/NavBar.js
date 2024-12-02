d7ea5b3f537be47dd79e6922da0e88085ec5933b
Michele De Guzman Murata <106496883+mmurata22@users.noreply.github.com>
2024-12-02 01:20:25 -0500

    Navbar functionality

--- tangerines/src/pages/navbar/NavBar.js
+++ tangerines/src/pages/navbar/NavBar.js
@@ -3,6 +3,11 @@ import React from 'react';
 import './NavBar.css';
 
 import logo from "../../assets/images/Logo.svg";
+import searchIcon from "../../assets/images/Search Icon.svg";
+import MyRecipeIcon from '../../assets/images/Recipe Button.svg';
+import ExploreRecipesIcon from '../../assets/images/Food Button.svg';
+import ViewChefsIcon from '../../assets/images/View Chefs Button.svg';
+import HomeIcon from '../../assets/images/On Hover.svg';
 
 function NavBar() {
     return(
@@ -10,7 +15,7 @@ function NavBar() {
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
           {/* NavBar Logo */}
           <a class="navbar-brand" href="/">
-            <img class="logo" src={logo} width="30" height="30" alt="Logo" />
+            <img class="logo" src={logo} alt="Logo" />
           </a>
       
           {/* Toggle button for smaller screens */}
@@ -20,22 +25,38 @@ function NavBar() {
       
           {/* Navbar links */}
           <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav mx-auto">
+
               <li class="nav-item active">
-                <a class="nav-link" href="#">Search <span class="sr-only">(current)</span></a>
+                <a class="navbar-brand" href="../Search">
+                    <img class="search-icon" src={searchIcon} alt="" />
+                </a>
               </li>
+
               <li class="nav-item">
-                <a class="nav-link" href="#">My Recipes</a>
+                <a class="nav-link" href="#">
+                <img class="my-recipe-icon" src={MyRecipeIcon} alt="" />
+                </a>
               </li>
+
               <li class="nav-item">
-                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link" href="#">
+                <img class="home-icon" src={HomeIcon} alt="" />
+                </a>
               </li>
+
               <li class="nav-item">
-                <a class="nav-link" href="#">Explore Recipes</a>
+                <a class="nav-link" href="#">
+                <img class="explore-recipe-icon" src={ExploreRecipesIcon} alt="" />
+                </a>
               </li>
+
               <li class="nav-item">
-                <a class="nav-link" href="#">Explore Chefs</a>
+                <a class="nav-link" href="#">
+                <img class="view-chefs-icon" src={ViewChefsIcon} alt="" />
+                </a>
               </li>
+
             </ul>
           </div>
         </nav>
